eafe5ba794ccb7a91949f651c33cff6aaef3558f
HerculesCamara <herculescamara2013@gmail.com>
2025-08-12 09:32:48 -0300

    feat: adding alembic to the project

--- fastapi-docker-postgres/app/database.py
+++ fastapi-docker-postgres/app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 # Aqui estou pegando a URL do db
--- fastapi-docker-postgres/app/models.py
+++ fastapi-docker-postgres/app/models.py
@@ -1,9 +1,14 @@
 from sqlalchemy import Column, Integer, String
 from .database import Base
 
+# O 'Base' é o cara que vai permitir traduzir a classe em python para criação de tabelas no bd
 class Item(Base):
   __tablename__ = "items"
 
   id = Column(Integer, primary_key=True, index=True)
   name = Column(String, index=True)
   description = Column(String, index=True, nullable=True)
+
+  def __init__(self, name: str, description: str = None):
+    self.nome = name
+    self.description = description
