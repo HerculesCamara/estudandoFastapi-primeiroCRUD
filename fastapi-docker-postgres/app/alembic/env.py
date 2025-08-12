ef7292a4ac6825e528f68d5b4aa4da792797ad71
HerculesCamara <herculescamara2013@gmail.com>
2025-08-12 13:19:33 -0300

    migration file

--- fastapi-docker-postgres/app/alembic/env.py
+++ fastapi-docker-postgres/app/alembic/env.py
@@ -23,6 +23,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
+from dotenv import load_dotenv
+load_dotenv()
+
 from database import Base
 target_metadata = Base.metadata
 
